b4f0dd5b3ea0e7f562c8d2d2a79d4776287d1142
Matthew Irwin <122063519+mattirwin1231@users.noreply.github.com>
2023-06-28 18:24:52 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// var halfsandwichprice = 9.99;
+// var tax = 0.07;
+// var sFee = 0.01;
+
+
+// function totalAll(){
+//   var num=document.getElementsByClassName("add");
+//     amount=0;
+//     for(var i=0;i<num.length;i++) {
+//     if (num[i].type==="checkbox"&&num[i].checked===true) {
+//         amount++;
+//     }
+//     }
+//   var sandwichTotal = amount * halfsandwichprice;
+//   var taxRate = sandwichTotal * tax;
+//   var afterTax = sandwichTotal + taxRate;
+//   var feeRate = sandwichTotal * sFee;
+//   var itemTotal = afterTax + feeRate; 
+  
+//   var res = document.getElementById('outputArea');
+//   res.innerHTML = ("<h2>Your total order is $" + itemTotal.toFixed(2) + " after taxes and fees.</h2>");
+  
+// }
+
